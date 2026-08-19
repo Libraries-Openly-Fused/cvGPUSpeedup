0f2cab987cba501a4a24e28954116c8a9a836a00
Oscar Amoros Huguet <morousg@gmail.com>
2026-08-19 23:27:31 +0200

    Update copyright

--- tests/batchread/test_circularbatchread_x_write3D.cu
+++ tests/batchread/test_circularbatchread_x_write3D.cu
@@ -1,5 +1,5 @@
 ﻿/* Copyright 2023 Mediaproduccion S.L.U. (Oscar Amoros Huguet)
-   Copyright 2025 Oscar Amoros Huguet
+   Copyright 2025-2026 Oscar Amoros Huguet
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
